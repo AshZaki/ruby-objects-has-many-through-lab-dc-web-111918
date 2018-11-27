bd1170374c3883f1973c03144767176c8e4bc801
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-27 17:39:12 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,14 +4,15 @@ class Patient
   
   @@all = []
   
+   def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
   def self.all
     @@all
   end
   
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
   
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
